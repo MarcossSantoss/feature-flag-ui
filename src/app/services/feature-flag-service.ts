3276867e334bdbd5170e170e5dbd9911567dbf16
Marcos Santos <marcosvbsantos26@gmail.com>
2026-02-03 22:50:47 -0300

    feat: initial angular project setup

---
+++ src/app/services/feature-flag-service.ts
@@ -0,0 +1,18 @@
+import { environment } from './../../environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FeatureFlagService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPercentage() {
+    return this.http.get<number>(`${this.baseUrl}/flags`);
+  }
+
+}
